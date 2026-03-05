e511a8c5f44c06eb53a7ffe7b437b58b11521ebb
Mayara Zucheli <131776167+MayaraZucheli@users.noreply.github.com>
2026-03-05 14:53:00 -0300

    Add files via upload

---
+++ 023.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+n = str(n)
+print(f'Analisando o numero {n}...')
+print(f'Unidade: {n[3]}')
+print(f'Dezena: {n[2]}')
+print(f'Centena: {n[1]}')
+print(f'Milhar: {n[0]}')
